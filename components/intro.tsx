a18b6f718753beceab345292ef61ad3364428b82
Kunal Manchanda <kunal.manchanda9999@gmail.com>
2024-09-18 10:12:35 +0530

    chore: update CV

--- components/intro.tsx
+++ components/intro.tsx
@@ -82,7 +82,7 @@ export default function Intro() {
 				</Link>
 				<a
 					className='group bg-white text-gray-900 p-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
-					href='/CV.pdf'
+					href='/KUNAL_MANCHANDA_CV.pdf'
 					download
 				>
 					Download CV <HiDownload className='opacity-60 group-hover:translate-y-1' />
